86e52ffc86be630aee086bebda616a117a9fb013
Fikri Fadillah <fkrfdllh@gmail.com>
2022-08-22 15:18:49 +0700

    Add io.jsonwebtoken in pom.xml

--- src/main/java/com/example/SimpleSpringSecurityProject/AuthController.java
+++ src/main/java/com/example/SimpleSpringSecurityProject/AuthController.java
@@ -7,7 +7,6 @@ import com.example.SimpleSpringSecurityProject.models.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
