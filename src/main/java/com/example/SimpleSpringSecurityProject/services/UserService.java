fb81be8139e233f566ccce032c1e081a55f4cc16
Fikri Fadillah <fkrfdllh@gmail.com>
2022-08-22 11:20:26 +0700

    Add UserService then configure it

---
+++ src/main/java/com/example/SimpleSpringSecurityProject/services/UserService.java
@@ -0,0 +1,31 @@
+package com.example.SimpleSpringSecurityProject.services;
+
+import com.example.SimpleSpringSecurityProject.models.UserModel;
+import com.example.SimpleSpringSecurityProject.models.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        UserModel foundedUser = userRepository.findByUsername(username);
+
+        if (foundedUser == null) {
+            return null;
+        }
+
+        String userUsername = foundedUser.getUsername();
+        String userPassword = foundedUser.getPassword();
+
+        return new User(userUsername, userPassword, null);
+    }
+}
