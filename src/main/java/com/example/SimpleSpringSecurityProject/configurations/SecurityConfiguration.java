e036cc60ea306747bddbc1f83683244785879415
Fikri Fadillah <fkrfdllh@gmail.com>
2022-08-22 11:29:50 +0700

    Configure SecurityConfiguration

--- src/main/java/com/example/SimpleSpringSecurityProject/configurations/SecurityConfiguration.java
+++ src/main/java/com/example/SimpleSpringSecurityProject/configurations/SecurityConfiguration.java
@@ -1,8 +1,37 @@
 package com.example.SimpleSpringSecurityProject.configurations;
 
+import com.example.SimpleSpringSecurityProject.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private UserService userService;
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userService);
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable().authorizeRequests().antMatchers("/subs", "/auth").permitAll().anyRequest().authenticated();
+    }
+
+    public PasswordEncoder passwordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
+    }
+
+    @Override
+    protected AuthenticationManager authenticationManager() throws Exception {
+        return super.authenticationManager();
+    }
 }
