6733f21c3deea0ef8a410edaf889a599a4ffeca8
Fikri Fadillah <fkrfdllh@gmail.com>
2022-08-22 11:35:26 +0700

    Configure authenticateClient in AuthController

--- src/main/java/com/example/SimpleSpringSecurityProject/AuthController.java
+++ src/main/java/com/example/SimpleSpringSecurityProject/AuthController.java
@@ -4,9 +4,11 @@ import com.example.SimpleSpringSecurityProject.models.AuthenticationRequest;
 import com.example.SimpleSpringSecurityProject.models.AuthenticationResponse;
 import com.example.SimpleSpringSecurityProject.models.UserModel;
 import com.example.SimpleSpringSecurityProject.models.UserRepository;
-import jdk.jfr.internal.EventWriter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,9 +21,21 @@ public class AuthController {
     @Autowired
     private UserRepository userRepository;
 
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
     @PostMapping("/auth")
     private ResponseEntity<?> authenticateClient(@RequestBody AuthenticationRequest authenticationRequest) {
+        String username = authenticationRequest.getUsername();
+        String password = authenticationRequest.getPassword();
 
+        try {
+            authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(username, password));
+        } catch (BadCredentialsException ex) {
+            return ResponseEntity.ok(new AuthenticationResponse("Error during client authentication"));
+        }
+
+        return ResponseEntity.ok(new AuthenticationResponse("Client authenticated: " + username));
     }
 
     @PostMapping("/subs")
