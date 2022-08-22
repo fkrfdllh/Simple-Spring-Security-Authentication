72924561be26bbe2b514d136ffc60e824e905520
Fikri Fadillah <fkrfdllh@gmail.com>
2022-08-22 16:08:19 +0700

    Fix bug by add @Component in JwtFilterRequest

--- src/main/java/com/example/SimpleSpringSecurityProject/services/JwtFilterRequest.java
+++ src/main/java/com/example/SimpleSpringSecurityProject/services/JwtFilterRequest.java
@@ -5,6 +5,7 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import javax.servlet.FilterChain;
@@ -13,6 +14,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+@Component
 public class JwtFilterRequest extends OncePerRequestFilter {
     @Autowired
     private JwtUtils jwtUtils;
