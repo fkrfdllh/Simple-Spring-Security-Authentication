72263de1336c58390313f9686e8f92f55c45fc88
Fikri Fadillah <fkrfdllh@gmail.com>
2022-08-22 10:26:01 +0700

    Initialize project

---
+++ src/main/java/com/example/SimpleSpringSecurityProject/SimpleSpringSecurityProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.SimpleSpringSecurityProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleSpringSecurityProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleSpringSecurityProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/SimpleSpringSecurityProject/SimpleSpringSecurityProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SimpleSpringSecurityProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleSpringSecurityProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
