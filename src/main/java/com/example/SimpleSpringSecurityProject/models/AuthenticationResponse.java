a7086e89161300ec8fa2229ac009532fc181e097
Fikri Fadillah <fkrfdllh@gmail.com>
2022-08-22 10:42:15 +0700

    Configure Authentication Response

--- src/main/java/com/example/SimpleSpringSecurityProject/models/AuthenticationResponse.java
+++ src/main/java/com/example/SimpleSpringSecurityProject/models/AuthenticationResponse.java
@@ -1,4 +1,16 @@
 package com.example.SimpleSpringSecurityProject.models;
 
 public class AuthenticationResponse {
+    private String response;
+
+    public String getResponse() {
+        return response;
+    }
+
+    public void setResponse(String response) {
+        this.response = response;
+    }
+
+    public AuthenticationResponse() {
+    }
 }
